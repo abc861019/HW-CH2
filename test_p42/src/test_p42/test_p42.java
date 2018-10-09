bd40a8953e7570a3947dee3b7795573d26438f75
Oscar <qazwer57977515@gmail.com>
2018-10-09 09:22:57 +0800

    Project init

---
+++ test_p42/src/test_p42/test_p42.java
@@ -0,0 +1,15 @@
+package test_p42;
+
+public class test_p42 {
+
+	public static void main(String[] args)
+	{
+	int [] test = {80, 60, 22, 50, 75, 85};
+	
+	for(int i=0;i<6;i++)
+	{
+		System.out.println("第" +(i+1) +"個人的成績是" +test[i]+ "分");
+	}
+	
+	}
+}
